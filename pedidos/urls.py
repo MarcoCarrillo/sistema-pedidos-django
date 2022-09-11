0265815420a16fe686b86ecffa8b5b8d690596a0
Marco <marcocarrilloosuna@gmail.com>
2022-09-10 21:38:30 -0500

    :sparkles: :memo: feat: CRUD Usuarios
    Creacion de la capa de persistencia de usuarios, como tambien de la documentacion de la API para este modelo

--- pedidos/urls.py
+++ pedidos/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from functools import cache
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+
+from users.api.router import router_user
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -35,4 +37,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/', include(router_user.urls)),
 ]
