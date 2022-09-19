eda12cbfa60adc007a1d99e6104242fc0a6a5994
Marco <marcocarrilloosuna@gmail.com>
2022-09-18 23:33:06 -0500

    :sparkles: feat: Creacion del ModelViewSet de Categorias
    Se realizo el CRUD y documentacion del modelo de Categorias

--- pedidos/urls.py
+++ pedidos/urls.py
@@ -20,6 +20,7 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 from users.api.router import router_user
+from categories.api.router import router_category
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -37,6 +38,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('api/', include(router_user.urls)),
     path('api/', include('users.api.router')),
+    path('api/', include(router_user.urls)),
+    path('api/', include(router_category.urls)),
 ]
