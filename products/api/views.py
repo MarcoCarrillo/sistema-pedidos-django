650474e0adc6df2e6b9a6d1f97c4ff401daff533
Marco <marcocarrilloosuna@gmail.com>
2022-10-04 22:17:28 -0500

    :rocket: feat: filtros de productos
    Permitir que los productos se filtren por estatus activo o inactivo y por categoria

--- products/api/views.py
+++ products/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
 
 from products.models import Product
 from products.api.serializers import ProductSerializer
@@ -8,3 +9,5 @@ class ProductApiViewSet(ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['category', 'active']
