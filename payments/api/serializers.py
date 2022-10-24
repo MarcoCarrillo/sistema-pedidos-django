1bb3c94f6894b4744ed70c73def775144414f04b
Marco <marcocarrilloosuna@gmail.com>
2022-10-23 21:21:16 -0500

    :rocket: feat: CRUD y documentacion de cuentas/pagos

---
+++ payments/api/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework.serializers import ModelSerializer
+
+from payments.models import Payment
+
+class PaymentSerializer(ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = ['id', 'table', 'totalPayment', 'paymentType', 'statusPayment', 'created_at']
---
+++ payments/api/views.py
@@ -0,0 +1,8 @@
+from rest_framework.viewsets import ModelViewSet
+
+from payments.models import Payment
+from payments.api.serializers import PaymentSerializer
+
+class PaymentApiViewSet(ModelViewSet):
+    serializer_class = PaymentSerializer
+    queryset = Payment.objects.all()
